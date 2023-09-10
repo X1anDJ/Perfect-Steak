4ae9421c69fcb09162b3db2c068aa491fd490dc9
Dajun <dxian780@gmail.com>
2023-09-10 15:31:59 -0700

    Added DerivedData to .gitignore

--- PerfectSteak/DerivedData/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/SnapKit.build/Objects-normal/x86_64/SnapKit-dummy.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/dajunxian/Developer/PerfectSteak/WithUserGuide/Perfect-Steak/PerfectSteak/DerivedData/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/SnapKit.build/module.modulemap \
-  /Users/dajunxian/Developer/PerfectSteak/WithUserGuide/Perfect-Steak/PerfectSteak/Pods/Target\ Support\ Files/SnapKit/SnapKit-dummy.m \
-  /Users/dajunxian/Developer/PerfectSteak/WithUserGuide/Perfect-Steak/PerfectSteak/Pods/Target\ Support\ Files/SnapKit/SnapKit-prefix.pch
--- PerfectSteak/DerivedData/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/SnapKit.build/Objects-normal/x86_64/SnapKit_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/dajunxian/Developer/PerfectSteak/WithUserGuide/Perfect-Steak/PerfectSteak/DerivedData/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/SnapKit.build/module.modulemap \
-  /Users/dajunxian/Developer/PerfectSteak/WithUserGuide/Perfect-Steak/PerfectSteak/DerivedData/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/SnapKit.build/DerivedSources/SnapKit_vers.c
